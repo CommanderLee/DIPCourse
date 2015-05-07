b3cc772280013374aa6b8e48bf53fc5db71c3525
Zhen <lizhen11@mails.tsinghua.edu.cn>
2015-05-07 10:39:44 +0800

    Load and Show images on the 1st tabControl

--- GroupPhotoProcessing/GroupPhotoProcessing/Form1.Designer.cs
+++ GroupPhotoProcessing/GroupPhotoProcessing/Form1.Designer.cs
@@ -31,14 +31,18 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPoint = new System.Windows.Forms.TabPage();
             this.panelImgNames = new System.Windows.Forms.Panel();
-            this.panelLoad = new System.Windows.Forms.Panel();
+            this.panelImgMethods = new System.Windows.Forms.Panel();
             this.labelImgName = new System.Windows.Forms.Label();
             this.buttonLoad = new System.Windows.Forms.Button();
             this.tabFusion = new System.Windows.Forms.TabPage();
             this.tabStiching = new System.Windows.Forms.TabPage();
+            this.panelImg = new System.Windows.Forms.Panel();
+            this.pictureBoxImg = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPoint.SuspendLayout();
-            this.panelLoad.SuspendLayout();
+            this.panelImgMethods.SuspendLayout();
+            this.panelImg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -54,8 +58,9 @@
             // 
             // tabPoint
             // 
+            this.tabPoint.Controls.Add(this.panelImg);
             this.tabPoint.Controls.Add(this.panelImgNames);
-            this.tabPoint.Controls.Add(this.panelLoad);
+            this.tabPoint.Controls.Add(this.panelImgMethods);
             this.tabPoint.Location = new System.Drawing.Point(4, 25);
             this.tabPoint.Name = "tabPoint";
             this.tabPoint.Padding = new System.Windows.Forms.Padding(3);
@@ -73,16 +78,16 @@
             this.panelImgNames.Size = new System.Drawing.Size(1530, 50);
             this.panelImgNames.TabIndex = 1;
             // 
-            // panelLoad
+            // panelImgMethods
             // 
-            this.panelLoad.BackColor = System.Drawing.Color.AliceBlue;
-            this.panelLoad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panelLoad.Controls.Add(this.labelImgName);
-            this.panelLoad.Controls.Add(this.buttonLoad);
-            this.panelLoad.Location = new System.Drawing.Point(10, 10);
-            this.panelLoad.Name = "panelLoad";
-            this.panelLoad.Size = new System.Drawing.Size(1530, 50);
-            this.panelLoad.TabIndex = 0;
+            this.panelImgMethods.BackColor = System.Drawing.Color.AliceBlue;
+            this.panelImgMethods.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelImgMethods.Controls.Add(this.labelImgName);
+            this.panelImgMethods.Controls.Add(this.buttonLoad);
+            this.panelImgMethods.Location = new System.Drawing.Point(10, 10);
+            this.panelImgMethods.Name = "panelImgMethods";
+            this.panelImgMethods.Size = new System.Drawing.Size(1530, 50);
+            this.panelImgMethods.TabIndex = 0;
             // 
             // labelImgName
             // 
@@ -90,9 +95,9 @@
             this.labelImgName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.labelImgName.Location = new System.Drawing.Point(100, 8);
             this.labelImgName.Name = "labelImgName";
-            this.labelImgName.Size = new System.Drawing.Size(81, 29);
+            this.labelImgName.Size = new System.Drawing.Size(55, 29);
             this.labelImgName.TabIndex = 1;
-            this.labelImgName.Text = "label1";
+            this.labelImgName.Text = "N/A";
             // 
             // buttonLoad
             // 
@@ -123,6 +128,25 @@
             this.tabStiching.Text = "ImageStiching";
             this.tabStiching.UseVisualStyleBackColor = true;
             // 
+            // panelImg
+            // 
+            this.panelImg.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panelImg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelImg.Controls.Add(this.pictureBoxImg);
+            this.panelImg.Location = new System.Drawing.Point(10, 130);
+            this.panelImg.Name = "panelImg";
+            this.panelImg.Size = new System.Drawing.Size(1530, 760);
+            this.panelImg.TabIndex = 2;
+            // 
+            // pictureBoxImg
+            // 
+            this.pictureBoxImg.Location = new System.Drawing.Point(15, -2);
+            this.pictureBoxImg.Name = "pictureBoxImg";
+            this.pictureBoxImg.Size = new System.Drawing.Size(1525, 755);
+            this.pictureBoxImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxImg.TabIndex = 0;
+            this.pictureBoxImg.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -134,8 +158,10 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPoint.ResumeLayout(false);
-            this.panelLoad.ResumeLayout(false);
-            this.panelLoad.PerformLayout();
+            this.panelImgMethods.ResumeLayout(false);
+            this.panelImgMethods.PerformLayout();
+            this.panelImg.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImg)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -146,10 +172,12 @@
         private System.Windows.Forms.TabPage tabPoint;
         private System.Windows.Forms.TabPage tabFusion;
         private System.Windows.Forms.TabPage tabStiching;
-        private System.Windows.Forms.Panel panelLoad;
+        private System.Windows.Forms.Panel panelImgMethods;
         private System.Windows.Forms.Panel panelImgNames;
         private System.Windows.Forms.Button buttonLoad;
         private System.Windows.Forms.Label labelImgName;
+        private System.Windows.Forms.Panel panelImg;
+        private System.Windows.Forms.PictureBox pictureBoxImg;
     }
 }
 
--- GroupPhotoProcessing/GroupPhotoProcessing/Form1.cs
+++ GroupPhotoProcessing/GroupPhotoProcessing/Form1.cs
@@ -15,6 +15,12 @@ namespace GroupPhotoProcessing
 {
     public partial class Form1 : Form
     {
+        // List of images and their control buttons
+        List<Image<Bgr, byte>>  imgList;
+        List<Button>            imgBtnList;
+
+        int                     currImgId;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +28,8 @@ namespace GroupPhotoProcessing
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            imgList = new List<Image<Bgr, byte>>();
+            imgBtnList = new List<Button>();
         }
 
         /// <summary>
@@ -35,7 +42,41 @@ namespace GroupPhotoProcessing
             OpenFileDialog openFile = new OpenFileDialog();
             if (openFile.ShowDialog() == DialogResult.OK)
             {
-                Image<Bgr, Byte> tmpImg = new Image<Bgr, byte>(openFile.FileName);
+                int btnId = imgList.Count;
+                Console.WriteLine("Button Id: " + btnId);
+
+                // Load and show
+                Image<Bgr, byte> tmpImg = new Image<Bgr, byte>(openFile.FileName);
+                pictureBoxImg.Image = tmpImg.ToBitmap();
+                imgList.Add(tmpImg);
+
+                // Add a button
+                // Magic numbers: Fixed button location/size in the 'panelImageNames'
+                Button tmpBtn = new Button();
+                tmpBtn.Text = btnId + ":" + openFile.FileName.Substring(openFile.FileName.Length - 8, 4);
+                tmpBtn.Name = btnId.ToString();
+                tmpBtn.Click += imgBtns_Click;
+                tmpBtn.Size = new Size(90, 45);
+                tmpBtn.Location = new Point(90 * btnId, 0);
+
+                panelImgNames.Controls.Add(tmpBtn);
+                imgBtnList.Add(tmpBtn);
+            }
+        }
+
+        private void imgBtns_Click(object sender, EventArgs e)
+        {
+            int btnId = Convert.ToInt32(((Button)sender).Name);
+            Console.WriteLine("Click on Button No." + btnId);
+            if (btnId >= 0 && btnId < imgList.Count)
+            {
+                pictureBoxImg.Image = imgList[btnId].ToBitmap();
+                currImgId = btnId;
+                labelImgName.Text = imgBtnList[btnId].Text + ": ";
+            }
+            else
+            {
+                Console.WriteLine("Error: Image Button Index out of range.");
             }
         }
     }
